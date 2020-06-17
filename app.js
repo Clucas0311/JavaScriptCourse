7a5c19b40610bc71a1be05115eb678c4dd346650
Clucas0311 <clucas0311@gmail.com>
2020-06-17 19:20:34 -0400

    Working on else statements cont.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ if (num % 2 !== 0);
 // 2 - meets expectations
 // 1 needs improvement
 // anything else - wtf?
-let rating = 213;
+/*let rating = 213;
 if (rating === 3) {
 	console.log("YOU'RE A SUPERSTAR!");
 } else if (rating === 2) {
@@ -22,4 +22,15 @@ if (rating === 3) {
 	console.log('NEEDS IMPROVEMENT');
 } else {
 	console('INVALID RATING!');
+}
+*/
+
+let highScore = 1430;
+let userScore = 1600;
+
+if (userScore >= highScore) {
+	console.log(`Congrats, you have a new high score of ${userScore}`);
+	highScore = userScore; // update highScore to the new userScore
+} else {
+	console.log(`Good Game. Your score of ${userScore} did not beat the high score of ${highScore}`);
 }
