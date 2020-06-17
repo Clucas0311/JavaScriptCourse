b019e4c41bff2a58b0945830f4a14f53e665d03b
Clucas0311 <clucas0311@gmail.com>
2020-06-17 18:29:07 -0400

    Practicing conditional statements

---
+++ app.js
@@ -0,0 +1,14 @@
+/*if (1 == 1) {
+	console.log(true);
+}*/
+
+/*let rating = 3;
+if (rating === 3) {
+	console.log("YOU'RE A SUPERSTAR!"); 
+}*/
+
+let num = 37;
+if (num % 2 !== 0);
+{
+	console.log('ODD NUMBER!');
+}
