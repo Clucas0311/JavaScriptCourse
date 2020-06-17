bc5bcedbadadbe90cbe8e6cc2736aafc35fc8faf
Clucas0311 <clucas0311@gmail.com>
2020-06-17 19:36:45 -0400

    Working on nested list

--- app.js
+++ app.js
@@ -1,36 +1,12 @@
-/*if (1 == 1) {
-	console.log(true);
-}*/
+let password = 'kitty';
 
-/*let num = 37;
-if (num % 2 !== 0);
-{
-	console.log('ODD NUMBER!');
-} */
-
-// Performance Review
-// 3 - superstar
-// 2 - meets expectations
-// 1 needs improvement
-// anything else - wtf?
-/*let rating = 213;
-if (rating === 3) {
-	console.log("YOU'RE A SUPERSTAR!");
-} else if (rating === 2) {
-	console.log('MEETS EXPECTATIONS');
-} else if (rating === 1) {
-	console.log('NEEDS IMPROVEMENT');
+if (password.length >= 6) {
+	if (password.indexOf(' ') === -1) {
+		// if it contains a whitespace it wont return -1 if it does it will and that will be true
+		console.log('Valid Password!');
+	} else {
+		console.log('Password is long enough, but cannot contain spaces');
+	}
 } else {
-	console('INVALID RATING!');
-}
-*/
-
-let highScore = 1430;
-let userScore = 1600;
-
-if (userScore >= highScore) {
-	console.log(`Congrats, you have a new high score of ${userScore}`);
-	highScore = userScore; // update highScore to the new userScore
-} else {
-	console.log(`Good Game. Your score of ${userScore} did not beat the high score of ${highScore}`);
+	console.log('Password must be longer!');
 }
