316b8eaeee4e1c33ab5f0f35e9e084bddbca7df6
Clucas0311 <clucas0311@gmail.com>
2020-06-17 20:25:48 -0400

    Working on logical Operators

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-let password = 'chicken Gal';
+/*let password = 'chicken Gal';
 if (password.length >= 8 && password.indexOf(' ') === -1) {
 	console.log('VALID PASSWORD');
 } else {
 	console.log('INVALID PASSWORD');
+} */
+
+let color = 'violet';
+
+if (color === 'purple' || color === 'orchid' || color === 'violet') {
+	console.log('GREAT CHOICE');
+} else {
+	console.log('UGLY COLOR!');
 }
