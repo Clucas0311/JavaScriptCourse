eeeb5457ef27ad30855ee6fd4055e74cacc6dc49
Clucas0311 <clucas0311@gmail.com>
2020-06-19 15:46:22 -0400

    Key Values

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
 let animals = [ 'sharks', 'salmon', 'whale', 'bear', 'lizard', 'tortoise' ];
 
-//let swimmers = animals.slice(0, 3);
-
-//let mammals = animals.slice(2, 4);
-
-let reptiles = animals.slice(4);
-
-let quadruped = animals.slice(-3);
-
-//console.log(swimmers);
-//console.log(mammals);
-console.log(reptiles);
-console.log(quadruped);
+animals.splice(1, 2);
+console.log(animals);
