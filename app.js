a95fc09d183f02d868e56d165a90fcbe21f8b267
Clucas0311 <clucas0311@gmail.com>
2020-06-18 20:31:43 -0400

    push methods

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-let shoopingList = [ 'cereal', 'cheese', 'ice' ];
+let topSongs = [ 'First Time I Ever Saw Your Face', 'God Only Knows', 'A Day In The Life', 'Life On Mars' ];
+
+topSongs.push('Confessions');
+console.log(topSongs);
