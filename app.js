5b088666eea737d8de2231918b5b9544e58360ce
Clucas0311 <clucas0311@gmail.com>
2020-06-19 18:44:46 -0400

    Looping through arrays and objects

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-for (let i = 1; i <= 20; i++) {
-	console.log(`${i} x ${i} = ${i * i}`);
+/*const word = 'stressed';
+for (i = 0; i < word.length; i++) {
+	console.log(word[i]);
+} */
+
+/*const word = 'stressed';
+for (i = word.length - 1; i >= 0; i--) {
+	console.log(word[i]);
+} */
+
+const myStudents = [
+	{ firstName: 'Zeus', grade: 86 },
+	{ firstName: 'Artemis', grade: 97 },
+	{ firstName: 'Hera', grade: 72 },
+	{ firstName: 'Apollo', grade: 90 }
+];
+for (i = 0; i < myStudents.length; i++) {
+	let students = myStudents[i];
+	console.log(`${students.firstName} scored ${students.grade}`);
 }
