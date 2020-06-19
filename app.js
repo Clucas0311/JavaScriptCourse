e5a1ef8fad4058ee741b55f9d7650cf7a3f47b10
Clucas0311 <clucas0311@gmail.com>
2020-06-18 21:23:47 -0400

    Array methods

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-let topSongs = [ 'First Time I Ever Saw Your Face', 'God Only Knows', 'A Day In The Life', 'Life On Mars' ];
+let animals = [ 'sharks', 'salmon', 'whale', 'bear', 'lizard', 'tortoise' ];
 
-topSongs.push('Confessions');
-console.log(topSongs);
+//let swimmers = animals.slice(0, 3);
+
+//let mammals = animals.slice(2, 4);
+
+let reptiles = animals.slice(4);
+
+let quadruped = animals.slice(-3);
+
+//console.log(swimmers);
+//console.log(mammals);
+console.log(reptiles);
+console.log(quadruped);
