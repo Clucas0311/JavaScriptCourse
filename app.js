3689dc8e00ae27902c548268fb9139105297f320
Clucas0311 <clucas0311@gmail.com>
2020-06-19 22:24:18 -0400

    reversing in loops

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-/*const word = 'stressed';
-for (i = 0; i < word.length; i++) {
-	console.log(word[i]);
-} */
+for (let char of 'cockledoodledoo') {
+	console.log(char.toUpperCase());
+}
 
-/*const word = 'stressed';
-for (i = word.length - 1; i >= 0; i--) {
-	console.log(word[i]);
-} */
+let subreddits = [ 'soccer', 'popheads', 'cringe', 'books' ];
 
-const myStudents = [
-	{ firstName: 'Zeus', grade: 86 },
-	{ firstName: 'Artemis', grade: 97 },
-	{ firstName: 'Hera', grade: 72 },
-	{ firstName: 'Apollo', grade: 90 }
-];
-for (i = 0; i < myStudents.length; i++) {
-	let students = myStudents[i];
-	console.log(`${students.firstName} scored ${students.grade}`);
+for (let sub of subreddits) {
+	console.log(sub);
 }
