4591dd1c25643fee8656028d465603c74a38a933
Clucas0311 <clucas0311@gmail.com>
2020-06-26 00:55:35 -0400

    Higher order functions return value

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const rage = () => {
-	console.log('I HATE EVERYTHING!');
+/* const rage = () => {
+	console.log('I HATE EVERYTHING');
 };
 
 const repeatNTimes = (action, num) => {
@@ -8,4 +8,32 @@ const repeatNTimes = (action, num) => {
 	}
 };
 
-repeatNTimes(rage, 19);
+repeatNTimes(rage, 19); */
+
+function multiplyBy(num) {
+	return function(x) {
+		return x * num;
+	};
+}
+const triple = multiplyBy(4);
+console.log(triple(4));
+
+const double = multiplyBy(6);
+console.log(double(2));
+
+function subtractBy(num) {
+	return function(x) {
+		return x - num;
+	};
+}
+const minus = subtractBy(7);
+console.log(minus(10));
+
+function divideBy(num) {
+	return function(x) {
+		return x / num;
+	};
+}
+
+const divide = divideBy(81);
+console.log(9);
