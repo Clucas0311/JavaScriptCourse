e6202f0e56421870a6300d087f8c938c09e9f03c
Clucas0311 <clucas0311@gmail.com>
2020-07-08 22:16:58 -0400

    Reducing under the hood

--- reducePractice.js
+++ reducePractice.js
@@ -1,8 +1,40 @@
-function reduce(array, combine, start) {
-	let current = start;
-	for (let element of array) {
-		current = combine(current, element);
+// Construct your own reduce function
+// that accepts an array, a callback, and an
+// initial value and returns a single value.
+
+//====> resulting into one single function
+// What does reduce do -
+// if you have an intial value set that equal to the number
+// loops through array -- and each element combines into one single item
+
+// declare a function called reduced - it has three parameters (array, callback, initialvalue)
+
+// function reduce(array, callback, intialValue) {
+// 	// initial value and current value is equaled to one another
+// 	let accumulator = intialValue;
+// 	for (let i = 0; i < array.length; i++) {
+// 		accumulator = callback(accumulator, array[i]);
+// 	}
+// 	return accumulator;
+// }
+
+function reduce(array, callback, initialValue) {
+	let accumulator;
+	if (initialValue === undefined) {
+		accumulator = array[0];
+		array = array.slice(1);
+	} else {
+		accumulator = initialValue;
 	}
-	return current;
+	for (let i = 0; i < array.length; i++) {
+		accumulator = callback(accumulator, array[i]);
+	}
+	return accumulator;
 }
-console.log(reduce([ 1, 2, 3, 4 ], (a, b) => a + b, 0));
+
+const testingReduce = reduce([ 1, 2, 3, 4, 5, 6 ], (total, currentVal) => total + currentVal);
+console.log(testingReduce);
+
+const grades = [ 87, 64, 96, 92, 88, 99, 73, 70, 64 ];
+const minGrade = reduce(grades, (min, currVal) => Math.min(min, currVal));
+console.log(minGrade);
