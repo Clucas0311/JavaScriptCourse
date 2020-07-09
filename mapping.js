ff2707bc1ac785b224fea6ba7f44a4f260287fc3
Clucas0311 <clucas0311@gmail.com>
2020-07-09 00:34:58 -0400

    Mapping again

--- mapping.js
+++ mapping.js
@@ -21,3 +21,18 @@ const doubleNums = map([ 20, 21, 22, 23, 24, 25, 26, 27 ], (numbs) => numbs * 2)
 const divideBy2 = map([ 12, 24, 36, 8, 20, 4 ], (number) => number / 2);
 console.log(divideBy2);
 console.log(doubleNums);
+
+const authors = [
+	{ firstName: 'Beatrix', lastName: 'Potter' },
+	{ firstName: 'Ann', lastName: 'Martin' },
+	{ firstName: 'Beverly', lastName: 'Cleary' },
+	{ firstName: 'Roald', lastName: 'Dahl' },
+	{ firstName: 'Lewis', lastName: 'Carroll' }
+];
+let fullAuthorNames;
+
+// fullAuthorNames should be: ["Beatrix Potter", "Ann Martin", "Beverly Cleary", "Roald Dahl", "Lewis Carroll"]
+// Write your code below
+
+fullAuthorNames = authors.map((fullName) => `${fullName.firstName} ${fullName.lastName}`);
+console.log(fullAuthorNames);
