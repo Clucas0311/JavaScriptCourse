b84a717ab5e668a6342431f08d253451a294182e
Clucas0311 <clucas0311@gmail.com>
2020-07-09 00:59:42 -0400

    Map and Filtering

--- mapFilter.js
+++ mapFilter.js
@@ -16,3 +16,57 @@ let displayYears;
 displayYears = years.filter((year) => year >= 2001).map((element) => `${element} A.D.`);
 
 console.log(displayYears);
+
+const userNames = [ 'Samir', 'Angela', 'Beatrice', 'Shaniqua', 'Marvin', 'Sean' ];
+
+// Result: [{name: 'Samir'}, {name: 'Shaniqua'}, {name:'Sean'}];
+
+const newObj = userNames.filter((user) => user.indexOf('S') === 0).map((name) => {
+	return { name: name };
+});
+
+console.log(newObj);
+
+const users = [
+	{ name: 'Samir', age: 27 },
+	{ name: 'Angela', age: 33 },
+	{ name: 'Beatrice', age: 42 },
+	{ name: 'Shaniqua', age: 30 },
+	{ name: 'Marvin', age: 23 },
+	{ name: 'Sean', age: 47 }
+];
+
+// filter and map to create an array with name strings with users thrity years and over
+
+const dirty30s = users.filter((user) => user.age >= 30).map((thirties) => `${thirties.name}`);
+console.log(dirty30s);
+
+const todos = [
+	{
+		todo: 'Buy apples',
+		done: false
+	},
+	{
+		todo: 'Wash car',
+		done: true
+	},
+	{
+		todo: 'Write web app',
+		done: false
+	},
+	{
+		todo: 'Read MDN page on JavaScript arrays',
+		done: true
+	},
+	{
+		todo: 'Call mom',
+		done: false
+	}
+];
+let unfinishedTasks;
+
+// unfinishedTasks should be: ["Buy apples", "Write web app", "Call mom"]
+// Write your code below
+
+unfinishedTasks = todos.filter((finished) => finished.done !== true).map((task) => task.todo);
+console.log(unfinishedTasks);
