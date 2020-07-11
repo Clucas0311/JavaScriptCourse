23da89d7e33c8bb161a3282be8a4d18dbaef4622
Clucas0311 <clucas0311@gmail.com>
2020-07-11 16:53:54 -0400

    reducing..

--- spreading.js
+++ spreading.js
@@ -8,3 +8,22 @@ function doubles(double, double1, double2, double3) {
 }
 
 const doubling = doubles(...nums);
+
+const cephalopods = [ 'dumbo octopus', 'humboldt squid', 'flamboyant cuttlefish' ];
+
+const gastropods = [ 'giant african snail', 'banana slug', 'variable neon slug' ];
+
+const cnidaria = [ 'fire coral', 'moon jelly' ];
+
+const mollusca = [ ...cephalopods, ...cnidaria ];
+
+console.log(mollusca);
+
+const movies = [
+	[ 'The Day the Earth Stood Still', 'Superman', 'Ghostbusters' ],
+	[ 'Finding Dory' ],
+	[ 'Jaws', 'On the Waterfront' ]
+];
+
+const flatMovies = movies.reduce((arr, innerMovies) => [ ...arr, ...innerMovies ], []);
+console.log(flatMovies);
