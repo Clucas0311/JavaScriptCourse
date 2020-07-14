d03f26b18a19a5730caeaa28e9a3cbe928fac808
Clucas0311 <clucas0311@gmail.com>
2020-07-13 23:16:33 -0400

    Practicing functions

---
+++ practicingProblems.js
@@ -0,0 +1,9 @@
+//Create a function addTwo that accepts one input and adds 2 to it.
+// addTwo(2) >> output should be 4
+
+// ToDo List
+// declare a function name it addTwo
+// This function one parameter ==> num
+//  return num + 2
+const addTwo = (num) => num + 2;
+console.log(addTwo(2));
