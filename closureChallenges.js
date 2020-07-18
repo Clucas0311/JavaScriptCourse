6ea6a508a016934c41d42e8f67787fa531b2aee4
Clucas0311 <clucas0311@gmail.com>
2020-07-18 14:38:25 -0400

    Closure challenge two

--- closureChallenges.js
+++ closureChallenges.js
@@ -14,5 +14,26 @@ function createFunction() {
 	};
 }
 
-const function1 = createFunction();
-function1(); // => should console.log('hello');
+// const function1 = createFunction();
+// function1(); // => should console.log('hello');
+
+// Challenge 2
+// Create a function createFunctionPrinter that accepts one input and returns a function.
+// When that created function is called, it should print out the input that was used when the
+// function was created.
+
+// create a function called createFunctionPrinter that function should accept one parameter
+// name the parameter input
+// inside the createFunctionPrinter's body return another function
+// inside the return function console.log the parameter
+
+function createFunctionPrinter(input) {
+	return function() {
+		console.log(input);
+	};
+}
+// /*** Uncomment these to check your work! ***/
+const printSample = createFunctionPrinter('sample');
+printSample(); // => should console.log('sample');
+const printHello = createFunctionPrinter('hello');
+printHello(); // => should console.log('hello');
