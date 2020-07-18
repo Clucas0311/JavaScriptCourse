164dd54a97fec5f8759c272a6278d5e0c89637bf
Clucas0311 <clucas0311@gmail.com>
2020-07-18 15:03:11 -0400

    Challenge 3

--- closureChallenges.js
+++ closureChallenges.js
@@ -60,9 +60,34 @@ const jasCounter = outer();
 // Before your do, guess what will be logged from each function call.
 
 // /*** Uncomment these to check your work! ***/
-willCounter();
-willCounter();
-willCounter();
+// willCounter();
+// willCounter();
+// willCounter();
 
-jasCounter();
-willCounter();
+// jasCounter();
+// willCounter();
+
+//Challenge 3
+// Examine the code for the outer function. Notice that we are returning a function
+// and that function is using variables that are outside of its scope.
+// Uncomment those lines of code. Try to deduce the output before executing.
+//  Now we are going to create a function addByX that returns a function that will
+//   add an input by x.
+function addByX(x) {
+	let counter = x;
+	return function(y) {
+		console.log(counter + y);
+	};
+}
+const addByTwo = addByX(2);
+addByTwo(1); // => should return 3
+addByTwo(2); // => should return 4
+addByTwo(3); // => should return 5
+
+const addByThree = addByX(3);
+addByThree(1); // => should return 4
+addByThree(2); // => should return 5
+
+const addByFour = addByX(4);
+addByFour(4); // => should return 8
+addByFour(5); // => should return 9
