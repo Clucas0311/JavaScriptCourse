4c27c9517c971e6970d682b8e95384f4f47bf534
Clucas0311 <clucas0311@gmail.com>
2020-07-20 17:13:41 -0400

    Ranging recursivly

--- recursion.js
+++ recursion.js
@@ -8,3 +8,10 @@ function countDown(num) {
 	countDown(num);
 }
 countDown(10);
+
+function sumRange(num) {
+	if (num === 1) return 1;
+	return num + sumRange(num - 1);
+}
+
+console.log(sumRange(4));
