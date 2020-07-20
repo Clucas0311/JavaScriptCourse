2ee2520c026834e275bf4326a1161744bbbc5652
Clucas0311 <clucas0311@gmail.com>
2020-07-20 19:15:18 -0400

    Recursion Power

--- recursion.js
+++ recursion.js
@@ -31,3 +31,36 @@ function factorial(num) {
 }
 
 console.log(factorial(3));
+
+function collectOddValues(arr) {
+	let result = [];
+	function helper(helperInput) {
+		if (helperInput.length === 0) {
+			return;
+		}
+		if (helperInput[0] % 2 !== 0) {
+			result.push(helperInput[0]);
+		}
+		helper(helperInput.slice(1));
+	}
+	helper(arr);
+	return result;
+}
+
+console.log(collectOddValues([ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]));
+
+function power(base, power) {
+	function inner(base, power) {
+		return base ^ power;
+	}
+	return inner;
+}
+console.log(power(2, 0));
+
+function power(base, exponent) {
+	if (exponent === 0) return 1;
+	return base * power(base, exponent - 1);
+}
+console.log(power(2, 0)); // 1
+console.log(power(2, 2)); // 4
+console.log(power(2, 4)); // 16
