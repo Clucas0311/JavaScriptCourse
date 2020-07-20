c27dcb0559d3339b43b41678f4d8d0e3ddd8b7f7
Clucas0311 <clucas0311@gmail.com>
2020-07-20 17:34:35 -0400

    Factorial 2

--- recursion.js
+++ recursion.js
@@ -16,11 +16,18 @@ function sumRange(num) {
 
 console.log(sumRange(4));
 
+// function factorial(num) {
+// 	let total = 1;
+// 	for (let i = num; i > 1; i--) {
+// 		total += i;
+// 	}
+// 	return total;
+// }
+// console.log(factorial(3));
+
 function factorial(num) {
-	let total = 1;
-	for (let i = num; i > 1; i--) {
-		total += i;
-	}
-	return total;
+	if (num === 1) return 1;
+	return num * factorial(num - 1);
 }
+
 console.log(factorial(3));
