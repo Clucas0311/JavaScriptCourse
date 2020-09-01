cd1bbd9ce4a93b0de4577552c83cc4dd6e72a0bb
Clucas0311 <clucas0311@gmail.com>
2020-08-31 22:03:19 -0400

    Check if array is array

--- arrays.js
+++ arrays.js
@@ -173,3 +173,23 @@ function applySplice(array, index, item1, item2) {
 	// return the array
 	return array;
 }
+
+// Determining if a value is an Array
+// Let us consider the case of wanting to determine if a value is an array
+// We can do so using the Array.isArray() method:
+
+// We are going to complete a function that takes in one parameter,
+// possibly an array, and returns whether the input in an array or not.
+// Your function should create a variable, assign it to an expression that
+// tells whether the input parameter is an array or not, using the Array.isArray()
+// method, then return that variable. Below are examples of the code running, assuming
+// that you will have completed the described function: isAnArray.
+
+function isAnArray(input) {
+	// create a result variable
+	let result;
+	// assign it to a call to the applicable method
+	result = Array.isArray(input);
+	// return the result variable
+	return result;
+}
