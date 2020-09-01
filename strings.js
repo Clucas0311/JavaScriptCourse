98a0a9737163086883697e2fde75b5bd207eb55a
Clucas0311 <clucas0311@gmail.com>
2020-08-31 20:30:12 -0400

    substring

--- strings.js
+++ strings.js
@@ -33,3 +33,26 @@ function applyIndexOf(string, subString) {
 	// return the index variable
 	return index;
 }
+
+// Let us consider the case of wanting to get a portion of a string
+// We can do so using the .substring() method (note -> there are other
+// ways to do this - research the .substr() method):
+
+//console.log('portion of string from before index 2, up to, but not including, index 9:', subString);
+// We are going to complete a function that takes in three parameters (string, start, end ->
+// where start and end are numerical indexes), and returns a portion of the string from
+// before start, up to, but not including, end. Your function should create a subString
+// variable and assign it to a call of the substring() method on the string, starting
+// from before start, up to, but not including, end, and return the subString variable.
+// Below are examples of the code running, assuming that you will have completed the
+// described function: applySubString.
+
+function applySubString(string, start, end) {
+	// create a subString variable
+	let subString;
+	// assign it to a portion of the string from before start, up to, but not including end
+	subString = string.substring(start, end);
+	// return the subString variable
+	return subString;
+}
+// console.log(applySubString('JavaScript'.substring(2, 9)));
