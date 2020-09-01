fba610b78fa8e908b1229acb8aa4607fc2aab26c
Clucas0311 <clucas0311@gmail.com>
2020-08-31 20:23:40 -0400

    IndexOf

--- strings.js
+++ strings.js
@@ -14,3 +14,22 @@ function applyToString(param) {
 }
 
 console.log(typeof applyToString(42));
+
+// Let us consider the case of wanting to determine where in a string we can find
+// a particular substringWe can do so using the .indexOf() method for string
+
+// We are going to complete a function that takes in two parameters (string, subString),
+//  and returns the index of the string where the subString can be found.
+//  Your function should create an index variable and assign it to a call of the
+//  indexOf() method on the string, and passing subString as an argument, and return
+//  the index variable. Below are examples of the code running, assuming that you will
+//  have completed the described function: applyIndexOf.
+
+function applyIndexOf(string, subString) {
+	// create an index variable
+	let index;
+	// assign it to the index inside of string where subString can first be found
+	index = string.indexOf(subString);
+	// return the index variable
+	return index;
+}
