ddc35a5fc36cfc7297f41ebad2ae361aeb462f41
Clucas0311 <clucas0311@gmail.com>
2020-09-01 16:29:29 -0400

    Creating an object

---
+++ objects.js
@@ -0,0 +1,13 @@
+// We are going to complete a function that takes no parameters,
+// and returns a newly created object. Your function should create a
+// variable and assign it to a new object, then return that variable.
+//  Below is an example of the code running, assuming that you will have
+//  completed the described function: createObject
+function createObject() {
+	// create a result variable,
+	let result;
+	// assign it to a new object
+	result = {};
+	// return the result variable
+	return result;
+}
