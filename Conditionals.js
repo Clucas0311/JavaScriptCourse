01ba02c2539139ce1c912d4ff03c8770adb68da9
Clucas0311 <clucas0311@gmail.com>
2020-09-02 20:39:31 -0400

    If else

--- Conditionals.js
+++ Conditionals.js
@@ -120,3 +120,46 @@ function generateReportSummary(reportTotals, teamName) {
 
 // var result3 = generateReportSummary({ gamma: 1, epsilon: 3, alpha: 4, bravo: 17 }, 'alpha');
 // console.log('Should log undefined, because function returned nothing:', result3);
+
+// if (/*some expression which evaluates to true or false*/) {
+
+//     /*some action to be taken in the event
+//     that the expression above evaluates to true*/
+
+//   } else {
+
+//     /*some action to be taken in the event
+//     that the expression above evaluates to false*/
+
+//   }
+
+// We are going to complete a function that takes in one parameter, determines if that parameter is
+// truthy or falsy, and returns a specific string for each case. Your function should use an if else
+// statement to determine if the input parameter is truthy, and if it is, should return the string
+// 'Input is truthy', or, if the input parameter is falsy, should return the string 'Input is falsy'.
+//  Below are examples of the code running, assuming that you will have completed the described function:
+//  isItTruthy.
+
+function isItTruthy(param) {
+	// if param is truthy
+	if (param) {
+		// return 'Input is truthy'
+		return 'Input is truthy';
+	} else {
+		// otherwise
+		// return 'Input is falsy'
+		return 'Input is falsy';
+	}
+}
+
+var result1 = isItTruthy('anything');
+console.log('should log "Input is truthy":', result1);
+
+var result2 = isItTruthy();
+console.log('should log "Input is falsy":', result2);
+
+var result3 = isItTruthy('');
+console.log('should log "Input is falsy":', result3);
+
+var result4 = isItTruthy(false);
+console.log('should log "Input is falsy":', result4);
