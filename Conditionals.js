d72c463818c696c19721c0b17bf84f5b1153fd62
Clucas0311 <clucas0311@gmail.com>
2020-09-02 20:46:13 -0400

    Comparison with Strings

--- Conditionals.js
+++ Conditionals.js
@@ -184,14 +184,39 @@ function balancePets(dogs, cats) {
 	}
 }
 
-var result1 = balancePets(9, 8);
-console.log('should log "number of cats and dogs is acceptable":', result1);
+// var result1 = balancePets(9, 8);
+// console.log('should log "number of cats and dogs is acceptable":', result1);
 
-var result2 = balancePets(7, 12);
-console.log('should log "number of cats or dogs is not acceptable":', result2);
+// var result2 = balancePets(7, 12);
+// console.log('should log "number of cats or dogs is not acceptable":', result2);
 
-var result3 = balancePets(7, 7);
-console.log('should log "number of cats or dogs is not acceptable":', result3);
+// var result3 = balancePets(7, 7);
+// console.log('should log "number of cats or dogs is not acceptable":', result3);
 
-var result4 = balancePets(5, 14);
-console.log('should log "number of cats or dogs is not acceptable":', result4);
+// var result4 = balancePets(5, 14);
+// console.log('should log "number of cats or dogs is not acceptable":', result4);
+
+// We are going to complete a function that takes in one parameter, a string representing a password,
+// determines whether the password is longer than 8 characters, and returns a specific string for each case.
+// Your function should use an if else statement to determine if the input string is long enough, and if it is,
+// should return the string 'Password is long enough', and if it is not, should return the string
+// 'Please enter a password of at least 9 characters'. Below are examples of the code running,
+// assuming that you will have completed the described function: passwordLongEnough.
+
+function passwordLongEnough(password) {
+	// if password length is greater than 8
+	if (password.length > 8) {
+		// return 'Password is long enough'
+		return 'Password is long enough';
+	} else {
+		// otherwise
+		// return 'Please enter a password of at least 9 characters'
+		return 'Please enter a password of at least 9 characters';
+	}
+}
+
+var result1 = passwordLongEnough('passafassaword');
+console.log('should log "Password is long enough":', result1);
+
+var result2 = passwordLongEnough('wordpass');
+console.log('Should log "Please enter a password of at least 9 characters":', result2);
