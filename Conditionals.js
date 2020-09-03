2c575cdde5030a89ed98440c81846180372ba619
Clucas0311 <clucas0311@gmail.com>
2020-09-02 20:14:41 -0400

    Working on less/greater than conditionals

--- Conditionals.js
+++ Conditionals.js
@@ -18,8 +18,29 @@ function isItUndefined(param) {
 		return `Input is defined`;
 	}
 }
-let result1 = isItUndefined('anything');
-console.log('should log "Input is defined":', result1);
+// let result1 = isItUndefined('anything');
+// console.log('should log "Input is defined":', result1);
 
-let result2 = isItUndefined();
+// let result2 = isItUndefined();
+// console.log('Should log undefined, because function returned nothing:', result2);
+
+// We are going to complete a function that takes in two parameters, both numbers representing totals
+// for apples and oranges, determines if there are fewer apples than oranges, and if so, returns
+// a specific string. Your function should use an if statement to determine if there are fewer apples
+//  than oranges, and if there are, should return the string 'There are fewer apples than oranges'.
+//  Below are examples of the code running, assuming that you will have completed the described function:
+//  fewerApples.
+
+function fewerApples(apples, oranges) {
+	// if there are fewer apples than oranges
+	if (apples < oranges) {
+		// return 'There are fewer apples than oranges'
+		return `There are fewer apples than oranges`;
+	}
+}
+
+var result1 = fewerApples(4, 7);
+console.log('should log "There are fewer apples than oranges":', result1);
+
+var result2 = fewerApples(19, 12);
 console.log('Should log undefined, because function returned nothing:', result2);
