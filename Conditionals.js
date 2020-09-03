b3ab12c52cbef471a9b56ab9e1650b01721d4fd3
Clucas0311 <clucas0311@gmail.com>
2020-09-02 20:23:58 -0400

    Searching if array contains an element - indexOf

--- Conditionals.js
+++ Conditionals.js
@@ -60,11 +60,35 @@ function validLengthPassword(password) {
 	}
 }
 
-var result1 = validLengthPassword('passwordyfurdy');
-console.log('should log "Password length is valid":', result1);
+// var result1 = validLengthPassword('passwordyfurdy');
+// console.log('should log "Password length is valid":', result1);
 
-var result2 = validLengthPassword('pass');
-console.log('Should log undefined, because function returned nothing:', result2);
+// var result2 = validLengthPassword('pass');
+// console.log('Should log undefined, because function returned nothing:', result2);
 
-var result3 = validLengthPassword('this is clearly too long of a password');
+// var result3 = validLengthPassword('this is clearly too long of a password');
+// console.log('Should log undefined, because function returned nothing:', result3);
+
+// We are going to complete a function that takes in two parameters, an array of agents in the field,
+//  and an agent to search for within that array, determines if the agent to search for is present
+//  within the array, and if it is, returns a specific string. Your function should use an if statement
+// to determine if the agent to search for is present within the list of agents, and if it is, should
+// return the string '{agentToSearchFor} is present within Agent list', where {agentToSearchFor} has
+// the value of the argument the function is called on. Below are examples of the code running, assuming
+// that you will have completed the described function: findAgent.
+
+function findAgent(agentList, agentToSearchFor) {
+	// if agentToSearchFor is present within agentList
+	if (agentList.indexOf(agentToSearchFor) > -1) {
+		// return '{agentToSearchFor} is present within Agent list'
+		return `${agentToSearchFor} is present within Agent list`;
+	}
+}
+var result1 = findAgent([ '001', '005', '007', '009' ], '007');
+console.log('should log "007 is present within Agent list":', result1);
+
+var result2 = findAgent([ 'tiny', 'teeny', 'eeny', 'meany' ], 'teeny');
+console.log('should log "teeny is present within Agent list":', result2);
+
+var result3 = findAgent([ 'red', 'blue', 'green' ], 'orange');
 console.log('Should log undefined, because function returned nothing:', result3);
