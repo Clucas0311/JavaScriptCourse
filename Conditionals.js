7dbdcae1329e49fac383bd9bf2b7e68db96d75cd
Clucas0311 <clucas0311@gmail.com>
2020-09-02 20:50:28 -0400

    Array else/if

--- Conditionals.js
+++ Conditionals.js
@@ -215,8 +215,35 @@ function passwordLongEnough(password) {
 	}
 }
 
-var result1 = passwordLongEnough('passafassaword');
-console.log('should log "Password is long enough":', result1);
+// var result1 = passwordLongEnough('passafassaword');
+// console.log('should log "Password is long enough":', result1);
 
-var result2 = passwordLongEnough('wordpass');
-console.log('Should log "Please enter a password of at least 9 characters":', result2);
+// var result2 = passwordLongEnough('wordpass');
+// console.log('Should log "Please enter a password of at least 9 characters":', result2);
+
+// We are going to complete a function that takes in two parameters, an array of ingredients for a recipe,
+// and an ingredient to search for within that array, determines whether the ingredient to search for is
+// present within the array, and returns a specific string for each case. Your function should use an if
+// else statement to determine if the ingredient to search for is present within the list of ingredients,
+// and if it is, should return the string '{ingredientToSearchFor} is on the list',
+// and if not, should return the string '{ingredientToSearchFor} is not on the list',
+// where {ingredientToSearchFor} has the value of the argument the function is called on.
+// Below are examples of the code running, assuming that you will have completed the described
+// function: findIngredient.
+function findIngredient(ingredientList, ingredientToSearchFor) {
+	// if ingredientToSearchFor is present within ingredientList
+	if (ingredientList.indexOf(ingredientToSearchFor) > -1) {
+		// return '{ingredientToSearchFor} is on the list'
+		return `${ingredientToSearchFor} is on the list`;
+		// otherwise
+	} else {
+		// return '{ingredientToSearchFor} is not on the list'
+		return `${ingredientToSearchFor} is not on the list`;
+	}
+}
+
+var result1 = findIngredient([ 'flour', 'butter', 'sugar', 'eggs' ], 'sugar');
+console.log('should log "sugar is on the list":', result1);
+
+var result2 = findIngredient([ 'milk', 'cereal', 'fruit' ], 'pop-tarts');
+console.log('should log "pop-tarts is not on the list":', result2);
