2862430552900099c555250381d4280fe4cbae48
Clucas0311 <clucas0311@gmail.com>
2020-09-04 18:30:49 -0400

    Iterating over a nested object with values of arrays

--- nestedLoops.js
+++ nestedLoops.js
@@ -137,3 +137,43 @@ loopAnArrayOfObjects([ { queue: false, stack: true }, { fish: 'swims' }, { shirt
 // s
 // p
 // e
+
+// Nested Mixed Loop (2)
+// Let us consider the case of wanting to loop over an array (or more than one),
+// inside of our looping over an object
+// We can so do using a for loop nested inside of a for-in loop
+
+// We are going to complete a function that takes one parameter, a object of arrays,
+// and logs all of its values (log each value in first inner array, one at a time,
+// then second inner array, and so on...) to the console. Your function should use a for
+// loop nested inside of a for-in loop to log each value from all inner arrays, then return nothing.
+// Below is an example of the code running, assuming that you will have completed the described function:
+// loopAnObjectOfArrays.
+
+function loopAnObjectOfArrays(objectOfArrays) {
+	// create a loop which iterates over the input object
+	for (let key in objectOfArrays) {
+		// create an inner loop which iterates over current inner array
+		for (let i = 0; i < objectOfArrays[key].length; i++) {
+			// log current value to the console
+			console.log(objectOfArrays[key][i]);
+		}
+	}
+}
+loopAnObjectOfArrays({ first: [ 1, 2, 5 ], second: [ 6, 14, 21 ] });
+// console output:
+// 1
+// 2
+// 5
+// 6
+// 14
+// 21
+
+loopAnObjectOfArrays({ third: [ false, false ], fourth: [ 'runs' ], ninth: [ 'q', 'l', 'z' ] });
+// console output:
+// false
+// false
+// runs
+// q
+// l
+// z
