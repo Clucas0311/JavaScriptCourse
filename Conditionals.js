f79e74205dd2fbe7d955e3a6364037fcb405299b
Clucas0311 <clucas0311@gmail.com>
2020-09-03 21:00:28 -0400

    Conditionals Else/if

--- Conditionals.js
+++ Conditionals.js
@@ -355,17 +355,60 @@ function selectShirtSize(choice) {
 		return 'You should select a different shirt';
 	}
 }
-var result1 = selectShirtSize(25);
-console.log('should log "You should select a size S":', result1);
+// var result1 = selectShirtSize(25);
+// console.log('should log "You should select a size S":', result1);
 
-var result2 = selectShirtSize(32);
-console.log('should log "You should select a size M":', result2);
+// var result2 = selectShirtSize(32);
+// console.log('should log "You should select a size M":', result2);
 
-var result3 = selectShirtSize(47);
-console.log('should log "You should select a size L":', result3);
+// var result3 = selectShirtSize(47);
+// console.log('should log "You should select a size L":', result3);
 
-var result4 = selectShirtSize(12);
-console.log('should log "You should select a different shirt":', result4);
+// var result4 = selectShirtSize(12);
+// console.log('should log "You should select a different shirt":', result4);
 
-var result5 = selectShirtSize(67);
-console.log('should log "You should select a different shirt":', result5);
+// var result5 = selectShirtSize(67);
+// console.log('should log "You should select a different shirt":', result5);
+
+function verifyStock(recipeMinimums, stockTomatoes, stockOnions) {
+	let excessTomatoes = stockTomatoes - recipeMinimums['tomatoes'];
+	let excessOnions = stockOnions - recipeMinimums['onions'];
+	// if stock of tomatoes and stock of onions are both less than minimum
+	if (excessTomatoes < 0 && excessOnions < 0) {
+		// return 'We need more tomatoes and more onions.'
+		return 'We need more tomatoes and more onions.';
+	} else if (excessTomatoes < 0 && excessOnions >= 0) {
+		// otherwise if stock of tomatoes is less than minimum but stock of onions is sufficient
+		// return 'We have enough onions, but need more tomatoes.'
+		return 'We have enough onions, but need more tomatoes.';
+	} else if (excessTomatoes >= 0 && excessOnions < 0) {
+		// otherwise if stock of tomatoes is sufficient but stock of onions is less than minimum
+		// return 'We have enough tomatoes, but need more onions.'
+		return 'We have enough tomatoes, but need more onions.';
+	} else {
+		// otherwise
+		// return 'We have enough tomatoes and onions. There will be {excessTomatoes} extra tomato, and {excessOnions} extra onion.'
+		return `We have enough tomatoes and onions. There will be ${excessTomatoes} extra tomato, and ${excessOnions} extra onion.`;
+	}
+}
+
+var result1 = verifyStock({ tomatoes: 3, onions: 7 }, 5, 8);
+console.log(
+	'should log "We have enough tomatoes and onions. There will be 2 extra tomato, and 1 extra onion.":',
+	result1
+);
+
+var result2 = verifyStock({ tomatoes: 5, onions: 1 }, 10, 4);
+console.log(
+	'should log "We have enough tomatoes and onions. There will be 5 extra tomato, and 3 extra onion.":',
+	result2
+);
+
+var result3 = verifyStock({ tomatoes: 2, onions: 4 }, 1, 1);
+console.log('should log "We need more tomatoes and more onions.":', result3);
+
+var result4 = verifyStock({ tomatoes: 4, onions: 2 }, 3, 4);
+console.log('should log "We have enough onions, but need more tomatoes.":', result4);
+
+var result5 = verifyStock({ tomatoes: 10, onions: 6 }, 11, 4);
+console.log('should log "We have enough tomatoes, but need more onions.":', result5);
