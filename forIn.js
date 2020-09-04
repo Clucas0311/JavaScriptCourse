c3f631a83d258eed1937d973203bd7bc7853766c
Clucas0311 <clucas0311@gmail.com>
2020-09-04 17:42:30 -0400

    Iteraring over properties

--- forIn.js
+++ forIn.js
@@ -37,3 +37,28 @@ loopOverKeys({ make: 'Ford', model: 'T', year: 1913 });
 // make
 // model
 // year
+
+// We are going to complete a function that takes one parameter, an object of properties,
+// and logs all of its values (one at a time) to the console. Your function should use a
+// for-in loop to log each value in the object, then return nothing. DO NOT USE Object.values()
+// in your solution. Below is an example of the code running, assuming that you will have completed
+// the described function: loopOverValues.
+
+function loopOverValues(object) {
+	// create a loop which iterates over the input object
+	for (let key in object) {
+		// log current value to the console
+		console.log(object[key]);
+	}
+}
+loopOverValues({ a: 1, b: 2, c: 3 });
+// console output:
+// 1
+// 2
+// 3
+
+loopOverValues({ make: 'Ford', model: 'T', year: 1913 });
+// console output:
+// Ford
+// T
+// 1913
