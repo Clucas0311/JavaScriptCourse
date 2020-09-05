2ef8b1eb8db517c8a28e3e78f3dbeca9fc71a58b
Clucas0311 <clucas0311@gmail.com>
2020-09-04 21:23:06 -0400

    Nested Loops

--- nestedLoops.js
+++ nestedLoops.js
@@ -160,20 +160,71 @@ function loopAnObjectOfArrays(objectOfArrays) {
 		}
 	}
 }
-loopAnObjectOfArrays({ first: [ 1, 2, 5 ], second: [ 6, 14, 21 ] });
-// console output:
-// 1
-// 2
-// 5
-// 6
-// 14
-// 21
+// loopAnObjectOfArrays({ first: [ 1, 2, 5 ], second: [ 6, 14, 21 ] });
+// // console output:
+// // 1
+// // 2
+// // 5
+// // 6
+// // 14
+// // 21
 
-loopAnObjectOfArrays({ third: [ false, false ], fourth: [ 'runs' ], ninth: [ 'q', 'l', 'z' ] });
+// loopAnObjectOfArrays({ third: [ false, false ], fourth: [ 'runs' ], ninth: [ 'q', 'l', 'z' ] });
+// // console output:
+// // false
+// // false
+// // runs
+// // q
+// // l
+// // z
+
+// All Combinations using For Loops
+// Let us consider the case of wanting all of the combinations possible between two arrays
+// We can do so by using a nest for loop:
+
+var numbers = [ 1, 2, 3 ];
+var letters = [ 'a', 'b', 'c' ];
+
+for (var i = 0; i < numbers.length; i++) {
+	for (var j = 0; j < letters.length; j++) {
+		console.log(numbers[i] + ' ' + letters[j]);
+	}
+}
+
+// We are going to complete a function that takes two parameters, both arrays,
+// and logs all possible combinations of elements separated by a space (see example for details...)
+// to the console. Your function should use a nested for loop to log all combinations of the two arrays,
+// then return nothing. Below is an example of the code running, assuming that you will have completed the
+// described function: generateCombinations.
+
+function generateCombinations(array1, array2) {
+	// create a loop which iterates over the first array
+	for (let i = 0; i < array1.length; i++) {
+		// create an inner loop which iterates over the second array
+		for (let j = 0; j < array2.length; j++) {
+			// log current element of first array and current element of second array to the console with space in between
+			console.log(array1[i] + ' ' + array2[j]);
+		}
+	}
+}
+
+generateCombinations([ 'a', 'b', 'c' ], [ 'd', 'e', 'f' ]);
 // console output:
-// false
-// false
-// runs
-// q
-// l
-// z
+// a d
+// a e
+// a f
+// b d
+// b e
+// b f
+// c d
+// c e
+// c f
+
+generateCombinations([ 1, 2 ], [ 'buckle', 'my', 'shoe' ]);
+// console output:
+// 1 buckle
+// 1 my
+// 1 shoe
+// 2 buckle
+// 2 my
+// 2 shoe
