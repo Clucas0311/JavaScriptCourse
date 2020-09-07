57878a2608f36fae09da0d2c28c4067fb5431e80
Clucas0311 <clucas0311@gmail.com>
2020-09-07 12:47:56 -0400

    Reassigning

--- objects.js
+++ objects.js
@@ -275,3 +275,18 @@ var obj = {
 };
 // var output = isPersonOldEnoughToVote(obj);
 // console.log(output); // --> true
+
+// Write a function called "addArrayProperty".
+// Given an object, a key, and an array, "addArrayProperty" sets a new property on the
+//  object at the given key, with its value set to the given array.
+
+function addArrayProperty(obj, key, arr) {
+	// object at the key reassign value to arr
+	obj[key] = arr;
+	// return arr
+	return obj;
+}
+var myObj = {};
+var myArray = [ 1, 3 ];
+addArrayProperty(myObj, 'myProperty', myArray);
+console.log(myObj.myProperty); // --> [1, 3]
