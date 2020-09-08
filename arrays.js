2f881f11bea16fe92153a83290b4b0471e7b50fd
Clucas0311 <clucas0311@gmail.com>
2020-09-08 17:45:36 -0400

    using reduce to find product of array

--- arrays.js
+++ arrays.js
@@ -389,7 +389,29 @@ function addToFrontOfNew(arr, element) {
 	// return newArr
 	return copyArr;
 }
-var input = [ 1, 2 ];
-var output = addToFrontOfNew(input, 3);
-console.log(output); // --> [3, 1, 2];
-console.log(input); // --> [1, 2]
+// var input = [ 1, 2 ];
+// var output = addToFrontOfNew(input, 3);
+// console.log(output); // --> [3, 1, 2];
+// console.log(input); // --> [1, 2]
+
+// Write a function called "computeProductOfAllElements".
+// Given an array of numbers, "computeProductOfAllElements" returns the
+// products of all the elements in the given array.
+// Notes:
+// If given array is empty, it should return 0.
+
+function computeProductOfAllElements(arr) {
+	//if arr length is zero
+	if (arr.length === 0) {
+		// then return 0
+		return 0;
+	}
+	// use reduce with the starting point = 0
+	return arr.reduce((prev, next) => {
+		return prev * next;
+	});
+	// add the acc + currentVal and return it
+}
+
+var output = computeProductOfAllElements([ 2, 5, 6 ]);
+console.log(output); // --> 60
