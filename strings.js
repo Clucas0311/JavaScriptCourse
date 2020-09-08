c762f1f838cc95877cb484db53318fe60f9f19b9
Clucas0311 <clucas0311@gmail.com>
2020-09-07 22:08:32 -0400

    indexOf from scratch

--- strings.js
+++ strings.js
@@ -65,3 +65,28 @@ function generateHaiku(firstLine, secondLine, thirdLine) {
 	// return the haiku variable
 	return haiku;
 }
+
+// Write a function called "getIndexOf".
+// Given a character and a string, "getIndexOf" returns the first position of the
+//  given character in the given string.
+// Notes:
+// Strings are zero indexed, meaning the first character in a string is at position 0.
+// When a string contains more than one occurrence of a character, it should return the
+// index of its first occurrence. If the character does not exist in the string,
+// it should return -1.
+// Do not use the native indexOf function in your implementation.
+function getIndexOf(char, str) {
+	// iterate through the str
+	for (let i = 0; i < str.length; i++) {
+		// if the str[i] equals char
+		if (str[i] === char) {
+			// return index
+			return i;
+		}
+	}
+	// return -1
+	return -1;
+}
+
+var output = getIndexOf('a', 'I am a hacker');
+console.log(output); // --> 2
