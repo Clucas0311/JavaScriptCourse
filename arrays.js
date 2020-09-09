02523c27b4bc8b33167bd8f88256757bb0ed5a6f
Clucas0311 <clucas0311@gmail.com>
2020-09-08 22:34:34 -0400

    new array element delete

--- arrays.js
+++ arrays.js
@@ -498,5 +498,36 @@ function findSmallestElement(arr) {
 	return min;
 }
 
-var output = findSmallestElement([ 4, 1, 9, 10 ]);
-console.log(output); // --> 1
+// var output = findSmallestElement([ 4, 1, 9, 10 ]);
+// console.log(output); // --> 1
+
+// Write a function called "removeElement".
+// Given an array of elements, and a "discarder" parameter, "removeElement"
+// returns an array containing the items in the given array that do not match the
+// "discarder" parameter.
+// Notes:
+// If all the elements match, it should return an empty array.
+// If an empty array is passed in, it should return an empty array.
+
+function removeElement(array, discarder) {
+	// if the array is empty
+	if (array.length === 0) {
+		// then return []
+		return [];
+	}
+	// create an empty array assign to result variable
+	const result = [];
+	// iterate through the array
+	for (let element of array) {
+		// if element doesn't equal discarder
+		if (element !== discarder) {
+			// push elements into result array
+			result.push(element);
+		}
+	}
+	// return result array
+	return result;
+}
+
+var output = removeElement([ 1, 2, 3, 2, 1 ], 2);
+console.log(output); // --> [1, 3, 1]
