9196b82a8bcbde2515906aec6db3e70b507c1aa1
Clucas0311 <clucas0311@gmail.com>
2020-09-17 17:49:05 -0400

    Find the smallest missing integer

--- codeWars.js
+++ codeWars.js
@@ -62,4 +62,25 @@ function index(array, n) {
 	// return Math.pow(array[n], n)
 	return Math.pow(array[n], n);
 }
-console.log(index([ 0, 1 ], 0));
+// console.log(index([ 0, 1 ], 0));
+
+// Hey awesome programmer!
+// You've got much data to manage and of course you use zero-based and non-negative ID's
+// to make each data item unique! Therefore you need a method, which returns the
+// smallest unused ID for your next new data item...
+// Note: The given array of used IDs may be unsorted. For test reasons there may be
+// duplicate IDs, but you don't have to find or remove them!
+// Go on and code some pure awesomeness!
+
+function nextId(ids) {
+	// create a variable and assign it to 0
+	let x = 0;
+	// while ids has x as an element run
+	while (ids.includes(x)) {
+		// increment x until it has a unknown
+		x++;
+	}
+	// return x
+	return x;
+}
+console.log(nextId([ 0, 1, 2, 3, 5 ])); // 4
