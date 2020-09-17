e6c84c189536998fc0bd50f5a6b4f32dffbc6ea9
Clucas0311 <clucas0311@gmail.com>
2020-09-17 15:02:07 -0400

    Creating an array from a num and iterate and push into array

--- codeWars.js
+++ codeWars.js
@@ -18,7 +18,7 @@ function hello(name) {
 // integer number is divisible by each out of two arguments.
 
 function isDivideBy(number, a, b) {
-	// if number is divisible by a and b
+	//if number is divisible by a and b
 	if (number % a === 0 && number % b === 0) {
 		// return true
 		return true;
@@ -29,3 +29,21 @@ function isDivideBy(number, a, b) {
 }
 console.log(isDivideBy(-12, 2, -6)); // true
 console.log(isDivideBy(-12, 2, -5)); // false;
+
+// We want an array, but not just any old array, an array with contents!
+// Write a function that produces an array with the numbers 0 to N-1 in it.
+// For example, the following code will result in an array containing the numbers 0 to 4:
+
+function arr(N) {
+	// create an empty array to push elements into
+	const container = [];
+	// iterate through the array starting at 0 ending at n
+	for (let i = 0; i < N; i++) {
+		// push elements into container
+		container.push(i);
+	}
+	// return empty array
+	return container;
+}
+
+console.log(arr(5));
