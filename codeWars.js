9cece284c1565eb9583746e668308de0c2d17738
Clucas0311 <clucas0311@gmail.com>
2020-09-19 16:28:32 -0400

    Slicing

--- codeWars.js
+++ codeWars.js
@@ -425,4 +425,13 @@ const reverseSeq = (n) => {
 	// return container
 	return container;
 };
-console.log(reverseSeq(5)); //[5, 4, 3, 2, 1])
+// console.log(reverseSeq(5)); //[5, 4, 3, 2, 1])
+
+// Create a method take that accepts a list/array and a number n, and
+// returns a list/array array of the first n elements from the list/array.
+function take(arr, n) {
+	// slice out the elements from 0 n plus 1
+	return arr.slice(0, n);
+}
+
+console.log(take([ 0, 1, 2, 3, 5, 8, 13 ], 3));
