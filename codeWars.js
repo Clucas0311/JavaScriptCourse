187166e3169c2dd64a21441f3c2fccbcf676e9a5
Clucas0311 <clucas0311@gmail.com>
2020-09-19 17:15:38 -0400

    Find the sum of the two smallest numbers

--- codeWars.js
+++ codeWars.js
@@ -455,3 +455,17 @@ function isPalindrome(x) {
 	return x.toLowerCase() === reversed;
 }
 console.log(isPalindrome('aba')); //true
+
+// Create a function that returns the sum of the two lowest positive
+// numbers given an array of minimum 4 positive integers. No floats or
+// non-positive integers will be passed.
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+function sumTwoSmallestNumbers(numbers) {
+	//Code here
+	// sort the array
+	let sorted = numbers.sort((a, b) => a - b);
+	// add the first and second indices and return it
+	return sorted[0] + sorted[1];
+}
+console.log(sumTwoSmallestNumbers([ 5, 8, 12, 19, 22 ])); // 13
