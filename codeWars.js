5951c09d2dd6263ce6a9eb013a0ff000880eb704
Clucas0311 <clucas0311@gmail.com>
2020-09-18 21:19:57 -0400

    Summation

--- codeWars.js
+++ codeWars.js
@@ -285,4 +285,18 @@ function check(a, x) {
 	// use the includes method
 	return a.includes(x);
 }
-console.log(check([ 66, 101 ], 66)); // true;
+// console.log(check([ 66, 101 ], 66)); // true;
+
+// Write a program that finds the summation of every number from 1 to num.
+// The number will always be a positive integer greater than 0.
+var summation = function(num) {
+	let sum = 0;
+
+	for (let i = 1; i <= num; i++) {
+		sum += i;
+	}
+
+	return sum;
+};
+
+console.log(summation(3)); // 6
