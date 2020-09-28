ab4f6e1a093d68e43b319b6bef26cf00ee73823e
Clucas0311 <clucas0311@gmail.com>
2020-09-28 13:40:36 -0400

    If number is divisble

--- 8kyu.js
+++ 8kyu.js
@@ -223,4 +223,12 @@ var array1 = [
 	true
 ];
 
-console.log(countSheeps(array1)); // 17
+// console.log(countSheeps(array1)); // 17
+
+// Create a function that checks if a number n is divisible by two numbers
+// x AND y. All inputs are positive, non-zero digits.
+
+function isDivisible(n, x, y) {
+	return n % x === 0 && n % y === 0 ? true : false;
+}
+console.log(isDivisible(12, 3, 4)); //true
