6b2b6b2382641a62c46ce0bf9e9c0f676e5cff22
Clucas0311 <clucas0311@gmail.com>
2020-09-28 15:21:11 -0400

    Switch statement

--- 8kyu.js
+++ 8kyu.js
@@ -231,4 +231,28 @@ var array1 = [
 function isDivisible(n, x, y) {
 	return n % x === 0 && n % y === 0 ? true : false;
 }
-console.log(isDivisible(12, 3, 4)); //true
+// console.log(isDivisible(12, 3, 4)); //true
+
+// Your task is to create a function that does four basic mathematical operations.
+// The function should take three arguments - operation(string/char), value1(number),
+// value2(number).
+// The function should return result of numbers after applying the chosen operation.
+function basicOp(operation, value1, value2) {
+	switch (operation) {
+		case '+':
+			return value1 + value2;
+			break;
+		case '-':
+			return value1 - value2;
+			break;
+		case '/':
+			return value1 / value2;
+			break;
+		case '*':
+			return value1 * value2;
+	}
+}
+console.log(basicOp('+', 4, 7)); //11
+console.log(basicOp('-', 15, 18)); //-3;
+console.log(basicOp('*', 5, 5)); //25
+console.log(basicOp('/', 49, 7)); //7
