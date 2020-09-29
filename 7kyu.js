7879f466bfeb3a39f42129b3d2574eeec8c88aaf
Clucas0311 <clucas0311@gmail.com>
2020-09-28 20:49:47 -0400

    Adding pars

--- 7kyu.js
+++ 7kyu.js
@@ -187,9 +187,32 @@ function arraySum(arr) {
 	return sum;
 }
 
-console.log(arraySum([ 1, 2, [ 1, 2 ] ])); //6
-console.log(arraySum([ 1, 2, 3 ])); //6
-console.log(arraySum([ 1, 2 ])); // 3
+// console.log(arraySum([ 1, 2, [ 1, 2 ] ])); //6
+// console.log(arraySum([ 1, 2, 3 ])); //6
+// console.log(arraySum([ 1, 2 ])); // 3
 
-const numArr = '1234'.split('');
-console.log(numArr);
+// I have the par value for each hole on a golf course and my stroke score on each hole.
+// I have them stored as strings, because I wrote them down on a sheet of paper. Right now,
+// I'm using those strings to calculate my golf score by hand: take the difference between
+// my actual score and the par of the hole, and add up the results for all 18 holes.
+
+// For example:
+
+// If I took 7 shots on a hole where the par was 5, my score would be: 7 - 5 = 2
+// If I got a hole-in-one where the par was 4, my score would be: 1 - 4 = -3.
+
+function golfScoreCalculator(parList, scoreList) {
+	// convert each list into a string then covert them into int
+	let par = parList.split('').map((p) => Number(p));
+	let score = scoreList.split('').map((s) => Number(s));
+	// create a total variable
+	let total = 0;
+	// iterate through the the par array
+	for (let i = 0; i < par.length; i++) {
+		// subtract each element and combine it it total
+		total += score[i] - par[i];
+	}
+	// return total
+	return total;
+}
+console.log(golfScoreCalculator('443454444344544443', '353445334534445344')); // -1
