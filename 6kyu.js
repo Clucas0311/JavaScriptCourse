3f9ad3b164ae2d6dcff94c75b5865d8ac96a85a6
Clucas0311 <clucas0311@gmail.com>
2020-10-09 16:09:10 -0400

    Outlier better

--- 6kyu.js
+++ 6kyu.js
@@ -4,17 +4,11 @@
 // except for a single integer N. Write a method that takes the array as an 
 // argument and returns this "outlier" N.
 function findOutlier(integers){
-    // create an array and filter out even and odd elements 
-    const even = integers.filter((element) => element % 2 === 0)
-    const odd = integers.filter((element) => element % 2 !== 0)
-    // if even is greater than odd
-    if (even.length === 1) {
-      // return the even[0]
-      return even[0];
-    }
-    // otherwise 
-      // return odd[0]
-    return odd[0];
+    // filter the integer for the even and odd elements
+  const even = integers.filter((element)=> element % 2 === 0);
+  const odd = integers.filter((element)=> element % 2 !== 0);
+  // if the length of even equals 1 return the even[0] otherwise odd[0]
+  return even.length === 1 ? even[0] : odd[0];
     }
 
 console.log(findOutlier([0, 1, 2])) // 1
