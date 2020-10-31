78e9e52523aac2abdf5fe5f5834c77b2d6017007
Clucas0311 <clucas0311@gmail.com>
2020-10-30 21:34:25 -0400

    divisbleBy

--- 8kyu.js
+++ 8kyu.js
@@ -417,28 +417,41 @@ function solution(a, b) {
 
 // console.log(solution('Soon', 'Me')); //'MeSoonMe'
 
-// All of the animals are having a feast! Each animal is bringing one dish. 
-// There is just one rule: the dish must start and end with the same letters 
-// as the animal's name. For example, the great blue heron is bringing garlic naan 
+// All of the animals are having a feast! Each animal is bringing one dish.
+// There is just one rule: the dish must start and end with the same letters
+// as the animal's name. For example, the great blue heron is bringing garlic naan
 // and the chickadee is bringing chocolate cake.
-// Write a function feast that takes the animal's name and dish as arguments 
-// and returns true or false to indicate whether the beast is allowed to bring the dish to 
-// the feast.Assume that beast and dish are always lowercase strings, 
-// and that each has at least two letters. 
-// beast and dish may contain hyphens and spaces, but these will not appear at the beginning 
-// or end of the string. 
+// Write a function feast that takes the animal's name and dish as arguments
+// and returns true or false to indicate whether the beast is allowed to bring the dish to
+// the feast.Assume that beast and dish are always lowercase strings,
+// and that each has at least two letters.
+// beast and dish may contain hyphens and spaces, but these will not appear at the beginning
+// or end of the string.
 // They will not contain numerals.
 
 function feast(beast, dish) {
-	// if the first character matches the first and last match character of dish 
-	return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1] 
+	// if the first character matches the first and last match character of dish
+	return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1];
 }
 
 //   console.log(feast("great blue heron", "garlic naan")) // true
 
 // Given a number n, return the number of positive odd numbers below n, EASY!
-function oddCount(n){
+function oddCount(n) {
 	return Math.floor(n / 2);
-  }
+}
 
- console.log(oddCount(15)) // 7
+// console.log(oddCount(15)); // 7
+
+//  Complete the function which takes two arguments and returns all numbers which
+//  are divisible by the given divisor. First argument is an array of numbers and the
+//  second is the divisor.
+
+function divisibleBy(numbers, divisor) {
+	// iterate using filter
+	// if the element is divisble by divisor
+	// return element
+	return numbers.filter((element) => element % divisor === 0);
+}
+
+console.log(divisibleBy([ 1, 2, 3, 4, 5, 6 ], 2)); // [2,4,6]
