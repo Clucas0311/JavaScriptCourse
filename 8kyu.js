956c994cf312f1479346ad551cf37e376975bc22
Clucas0311 <clucas0311@gmail.com>
2020-11-01 21:24:32 -0500

    Divisor

--- 8kyu.js
+++ 8kyu.js
@@ -454,4 +454,4 @@ function divisibleBy(numbers, divisor) {
 	return numbers.filter((element) => element % divisor === 0);
 }
 
-console.log(divisibleBy([ 1, 2, 3, 4, 5, 6 ], 2)); // [2,4,6]
+console.log(divisibleBy([ 1, 2, 3, 4, 5, 6 ], 3)); // [2,4,6]
