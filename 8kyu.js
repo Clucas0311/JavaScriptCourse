39bb425c6ae30fdfdad47273e74119b4985bbcba
Clucas0311 <clucas0311@gmail.com>
2020-11-02 23:13:01 -0500

    Range

--- 8kyu.js
+++ 8kyu.js
@@ -454,4 +454,22 @@ function divisibleBy(numbers, divisor) {
 	return numbers.filter((element) => element % divisor === 0);
 }
 
-console.log(divisibleBy([ 1, 2, 3, 4, 5, 6 ], 3)); // [2,4,6]
+// console.log(divisibleBy([ 1, 2, 3, 4, 5, 6 ], 2)); // [2,4,6]
+
+// Implement a function named generateRange(min, max, step), which
+// takes three arguments and generates a range of integers from min to max,
+// with the step. The first integer is the minimum value, the second is the
+// maximum of the range and the third is the step. (min < max)
+
+function generateRange(min, max, step) {
+	// create an array container to put elements into
+	const container = [];
+	// iterate start at min and end at max incrementing step
+	for (let i = min; i <= max; i += step) {
+		// push into container i
+		container.push(i);
+	}
+	// return container
+	return container;
+}
+console.log(generateRange(2, 10, 2)); // [2,4,6,8,10]
