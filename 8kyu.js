55eb2258a9280d96faba4c043f84bbbe29c703f6
Clucas0311 <clucas0311@gmail.com>
2020-12-07 23:33:19 -0500

    Sum

--- 8kyu.js
+++ 8kyu.js
@@ -3,9 +3,9 @@
 // The numbers can also be negative. If the array does not contain any numbers then you
 // should return 0.
 
-function sum(numbers) {
+const sum = (numbers) => {
 	return numbers.reduce((prev, next) => prev + next, 0);
-}
+};
 
 console.log(sum([1, 5.2, 4, 0, -1])); //9.2
 
